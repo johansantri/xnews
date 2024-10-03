f6119f51e2df56426535fccecfaa2aaab94bcf0b
johan <johansantri@gmail.com>
2024-10-03 10:58:53 +0700

    update struktur

--- authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-# Register your models here.
-from django.contrib.auth.models import User
-
-admin.site.register(Profile)
---
+++ core/authentication/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+# import this for sending email to user
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from authentication.forms import UserRegistrationForm
+from .models import Profile
+
+# Create your views here.
+
+@login_required(login_url='login')
+def homepage(request):
+    return redirect('/post_list')
+
+
+def register(request):
+    form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            # email user with activation link
+            current_site = get_current_site(request)
+            mail_subject = "Activate your account."
+
+            # the message will render what is written in authentication/email_activation/activate_email_message.html
+            message = render_to_string('authentication/email_activation/activate_email_message.html', {
+                    'user': form.cleaned_data['username'],
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token':  default_token_generator.make_token(user),
+                })
+            to_email = form.cleaned_data['email']
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            messages.success(request, 'Account created successfully. Please check your email to activate your account.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account creation failed. Please try again.')
+
+
+    return render(request, 'authentication/register.html',{
+        'form': form
+    })
+
+# to activate user from email
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return render(request, 'authentication/email_activation/activation_successful.html')
+    else:
+        return render(request, 'authentication/email_activation/activation_unsuccessful.html')
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request,'authentication/profile.html',{'profiles':profiles})
+    else:
+        return redirect('login')
+    
+def profile(request,pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            if action =="unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
+            
+        return render(request,'authentication/myprofile.html',{'profile':profile})
+    else:
+        return redirect('login')
+    
+    
